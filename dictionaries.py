36840f0033559587e9bab031c0385dbf804556fb
Rabina Roka <108641574+RabinaRoka@users.noreply.github.com>
2025-03-05 21:32:41 +0545

    lists and dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -15,3 +15,32 @@ inventory['pocket'] = ['seashell', 'strange berry', 'lint']
 inventory['backpack'].sort()
 inventory['backpack'].remove('dagger')
 inventory['gold'] = inventory['gold'] + 50
+
+
+#lists and dictionaries
+shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+  "banana": 6,
+  "apple": 0,
+  "orange": 32,
+  "pear": 15
+}
+    
+prices = {
+  "banana": 4,
+  "apple": 2,
+  "orange": 1.5,
+  "pear": 3
+}
+
+# Write your code below!
+def compute_bill(food):
+  total = 0
+  for item in food:
+    if stock[item] > 0:
+      total += prices[item]
+      stock[item] -= 1
+  return total
+
+  print total
